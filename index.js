ae08d0218cd17ff7a9065a7fd24682d8d500f7fb
Diogo vinícius <diogov1204@gmail.com>
2023-09-21 11:27:22 -0300

    dia 21/09 feito

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-button=document.getElementById("botao").addEventListener('click',function(){
-     fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+document.getElementById("botao")
+.addEventListener('click',function(){
+    
+    let nome = document.getElementById('nome-pokemon').value
+     fetch("https://pokeapi.co/api/v2/pokemon/" + nome)
     .then(function(response){
     return response.json()
 
     }).then(function(response){
-        console.log(response)
+        console.log(response.abilities)
+        for(const)
+        console.log(response.sprites.front_default)
         let url=response.sprites.front_default
         document.getElementById('img-card').setAttribute('src',url)
     })
